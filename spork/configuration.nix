4dbd6b717e2ffe6aa9185676abde7c0c68b4365b
Chris Dunphy <chris@megaparsec.ca>
2023-02-19 15:03:00 -0700

    Got Wayland working with nVidia

--- spork/configuration.nix
+++ spork/configuration.nix
@@ -49,8 +49,13 @@ in
     videoDrivers = [ "nvidia" ];
     layout = "us";
     xkbVariant = "";
+    displayManager.gdm.wayland = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
   hardware.opengl.enable = true;
+  hardware.nvidia.modesetting.enable = true;
+  programs.xwayland.enable = true;
 
   # Virtualization and Containers
   virtualisation = {
@@ -75,8 +80,6 @@ in
   };
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
