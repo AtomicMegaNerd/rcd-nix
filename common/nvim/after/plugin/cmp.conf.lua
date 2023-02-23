90ec06fa30f2732f24afc0b8dc1ada7f81b1779e
Chris Dunphy <chris@megaparsec.ca>
2023-02-23 12:57:34 -0700

    Fix oopsie in the NeoVim config

--- common/nvim/after/plugin/cmp.conf.lua
+++ common/nvim/after/plugin/cmp.conf.lua
@@ -12,7 +12,7 @@ end
 vim.o.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
-	mapping = cmp.mapping.preset.search({
+	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
