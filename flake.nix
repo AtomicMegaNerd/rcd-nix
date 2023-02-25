f1d9eb24243b740959148dfe20c2184452328bee
Chris Dunphy <chris@megaparsec.ca>
2023-02-25 12:50:28 -0700

    Minor cleanups of our flake

--- flake.nix
+++ flake.nix
@@ -2,43 +2,28 @@
   description = "AtomicMegaNerd Core Nix Flake";
 
   inputs = {
-    # We are going to use both stable and unstable packages
+    # NixOS stable and unstable sources
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    # Home Manager sources
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
     let
-      # 22.11 Stable
-      pkgs = import nixpkgs {
+      pkgs-linux = import nixpkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
-      # Unstable
-      unstable = import nixpkgs-unstable {
+      unstable-linux = import nixpkgs-unstable {
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
     in
     {
-      # Home Manager configuration
-      homeConfigurations = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        "rcd@blahaj" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable;
-          modules = [ ./blahaj/rcd.nix ];
-        };
-        "root@blahaj" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable;
-          modules = [ ./blahaj/rcd.nix ];
-        };
-      };
+
       # Nix OS core configuration
       nixosConfigurations = {
         blahaj = nixpkgs.lib.nixosSystem {
@@ -46,6 +31,20 @@
           modules = [
             ./blahaj/configuration.nix
           ];
+        };
+      };
+
+      # Home Manager configuration
+      homeConfigurations = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        "rcd@blahaj" = home-manager.lib.homeManagerConfiguration {
+          pkgs-linux = unstable-linux;
+          modules = [ ./blahaj/rcd.nix ];
+        };
+        "root@blahaj" = home-manager.lib.homeManagerConfiguration {
+          pkgs-linux = unstable-linux;
+          modules = [ ./blahaj/rcd.nix ];
         };
       };
     };
